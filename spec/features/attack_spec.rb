28ce19f620c45b794fd1a04968c5ab430c7a31e3
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-11-29 11:04:58 +0000

    Implement hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,15 @@
 feature "Attack player" do
   scenario "Attack player 2" do
     sign_in_and_play
-    click_link 'Attack player 2!'
+    click_button 'Attack!'
     expect(page).to have_content 'Melvin attacked Derrick!'
+  end
+
+  scenario "Attack player 2 should reduce player 2 HP by 10" do
+    sign_in_and_play
+    click_button 'Attack!'
+    click_button 'OK'
+    expect(page).not_to have_content "Derrick: 60HP"
+    expect(page).to have_content "Derrick: 50HP"
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,37 @@
 require "player"
 
 describe Player do
+  let(:player_1) { Player.new("Melvin") }
+  let(:player_2) { Player.new("Derrick") }
+
   describe "#name" do
     it "should return the player's name" do
-      player = Player.new("Melvin")
-      expect(player.name).to eq "Melvin"
+      expect(player_1.name).to eq "Melvin"
     end
 
   end
+
+  describe "#hit_points" do
+    it "should return the player's hit points" do
+      player = Player.new("Melvin")
+      expect(player_1.hit_points).to eq Player::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe "#attack" do
+    it "should take one argument" do
+      expect(player_1).to respond_to(:attack).with(1).argument
+    end
+
+    it "should reduce the other player's hit points by 10" do
+      expect { player_1.attack(player_2) }.to change { player_2.hit_points }.by -10
+    end
+
+  end
+
+
+  # describe "#receive_damage" do
+
+  # end
 
 end
